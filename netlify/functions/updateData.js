8951125eccfa56d74864d684b8fc13e63d27fbd2
octobervale <peanutbug04@gmail.com>
2025-02-02 23:55:50 -0700

    ;lkjhb

--- netlify/functions/updateData.js
+++ netlify/functions/updateData.js
@@ -8,13 +8,16 @@ exports.handler = async (event) => {
         console.log('Parsed event body:', { REPO_OWNER, REPO_NAME, FILE_PATH, data });
 
         const GITHUB_TOKEN = process.env.FP_ACCESS;
+        console.log("GitHub Token is set:", GITHUB_TOKEN ? "Yes" : "No");
+
         const message = 'Update data.json';
         const content = Buffer.from(JSON.stringify(data, null, 2)).toString('base64'); // Base64 encode the JSON data
 
         // Get the SHA of the existing file
         const fileResponse = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`, {
             headers: {
-                'Authorization': `token ${GITHUB_TOKEN}`
+                'Authorization': `Bearer ${GITHUB_TOKEN}`,
+                'Accept': 'application/vnd.github.v3+json'
             }
         });
 
@@ -32,7 +35,8 @@ exports.handler = async (event) => {
         const updateResponse = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`, {
             method: 'PUT',
             headers: {
-                'Authorization': `token ${GITHUB_TOKEN}`,
+                'Authorization': `Bearer ${GITHUB_TOKEN}`,
+                'Accept': 'application/vnd.github.v3+json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
