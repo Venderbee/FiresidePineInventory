695c3c10fd51488ce0f30edc714b2e0f432f5b1a
octobervale <peanutbug04@gmail.com>
2025-02-05 09:08:18 -0700

    utyrtd

--- netlify/functions/readData.js
+++ netlify/functions/readData.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 exports.handler = async () => {
     try {
-        const dataPath = path.join(__dirname, 'data.json');
+        const dataPath = path.resolve(__dirname, 'data.json');
+        console.log('Reading data from:', dataPath);
         const data = await fs.readFile(dataPath, 'utf-8');
+        console.log('Data read successfully');
         return {
             statusCode: 200,
             body: data,
@@ -13,7 +15,7 @@ exports.handler = async () => {
         console.error('Error reading data:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Error reading data' }),
+            body: JSON.stringify({ message: 'Error reading data', error: error.message }),
         };
     }
 };
--- netlify/functions/writeData.js
+++ netlify/functions/writeData.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 exports.handler = async (event) => {
     try {
-        const dataPath = path.join(__dirname, 'data.json');
+        const dataPath = path.resolve(__dirname, 'data.json');
+        console.log('Writing data to:', dataPath);
         const data = JSON.parse(event.body);
         await fs.writeFile(dataPath, JSON.stringify(data, null, 2), 'utf-8');
+        console.log('Data written successfully');
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Data saved successfully' }),
